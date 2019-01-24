e39510857633f48a52bf2a6a2f29dbfa0dd5fc73
Issam ASSAFI <issamassafi@gmail.com>
2019-01-24 13:32:28 +0100

    Work related to the media section, updated the base template to include personnalised scripts

--- Media/models.py
+++ Media/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Cours(models.Model):
+    title = models.CharField(max_length=150)
+    description= models.TextField()
+    category = models.CharField(max_length=150)
+    professeur = models.CharField(max_length=200)
+    support_pdf = models.CharField(max_length=100)
+    video_src = models.CharField(max_length=100,null=True)
+    def __str__(self):
+        return self.title
+
