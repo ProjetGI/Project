ca57e6afecff068b66d8c8f8138ee02f62bbdbf0
othmaneJaz <othmane.jaz@gmail.com>
2019-01-25 03:33:55 +0100

    Tables added and other forum changes

--- Forum/admin.py
+++ Forum/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Publication, Reponse
+
+admin.site.register(Publication)
+admin.site.register(Reponse)
--- Forum/views.py
+++ Forum/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from .models import Publication, Reponse
 # Create your views here.
 
+
+def reponse(request):
+    # obj = Reponse.objects.get(id=1)
+    # context = {
+    # "object":obj
+    # }
+    # return render(request,'Forum/reponse.html',context)
+    return render(request,'Forum/reponse.html')
+
 def forum(request):
-    return render(request,'Forum/forum.html',{})
+    liste_publications = Publication.objects.all()
+    context = {
+    "liste_publications":liste_publications
+    }
+    return render(request,'Forum/forum.html',context)
