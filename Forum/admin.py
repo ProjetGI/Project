f7641d8d30d32d5801e1b877ca7611d7287018c5
othmaneJaz <othmane.jaz@gmail.com>
2019-01-26 05:28:28 +0100

    forum in progress

--- Forum/admin.py
+++ Forum/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-
+from .forms import PublicationForm
 from .models import Publication, Reponse
 
-admin.site.register(Publication)
+
+
+class PublicationFormAdmin(admin.ModelAdmin):
+   form = PublicationForm
+   # class Meta:
+   # 	   model = Publication
+   	   
+
 admin.site.register(Reponse)
+admin.site.register(Publication, PublicationFormAdmin)
--- Forum/views.py
+++ Forum/views.py
@@ -1,15 +1,40 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView, DetailView,
+    CreateView,DeleteView )
+from .forms import PublicationForm
 from .models import Publication, Reponse
+from django.urls import reverse_lazy
+
 # Create your views here.
 
 
-def reponse(request):
-    # obj = Reponse.objects.get(id=1)
-    # context = {
-    # "object":obj
-    # }
-    # return render(request,'Forum/reponse.html',context)
-    return render(request,'Forum/reponse.html')
+class PublicationCreateView(CreateView):
+    queryset = Publication.objects.all()
+    form_class = PublicationForm
+    template_name = 'Forum/create_publication.html'
+    success_url = reverse_lazy('forum')
+    # fields = [
+    #         "titre",
+    #         "sujet"
+    #     ]
+
+class PublicationDeleteView(DeleteView):
+    model = Publication
+    template_name = 'Forum/confirm_delete_pub.html'
+    success_url = reverse_lazy('forum')
+    # fields = [
+    #         "titre",
+    #         "sujet"
+    #     ]
+
+def reponse(request, pk=4):
+    obj = Reponse.objects.get(pk=pk)
+    context = {
+    "object":obj
+    }
+    return render(request,'Forum/reponse.html',context)
+    # return render(request,'Forum/reponse.html')
 
 def forum(request):
     liste_publications = Publication.objects.all()
@@ -17,3 +42,13 @@ def forum(request):
     "liste_publications":liste_publications
     }
     return render(request,'Forum/forum.html',context)
+
+# def create_publication(request):
+
+def publication(request, pk=4):
+    obj = Publication.objects.get(pk=pk)
+    context = {
+    "object":obj
+    }
+    return render(request,'Forum/publication.html',context)
+    # return render(request,'Forum/reponse.html')
